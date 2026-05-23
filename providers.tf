6d56091775cfc6212d437ecbb1dac7bd31fbf209
Phan Đình Khải <174919444+khaipd18@users.noreply.github.com>
2026-05-23 22:05:34 +0700

    Add GitHub Actions workflow for Terraform deployment and Checkov security scan

--- providers.tf
+++ providers.tf
@@ -1,12 +1,18 @@
+provider "aws" {
+  region = var.region
+}
+
 terraform {
+  backend "s3" {
+    bucket         = "lab02-1-23520678"
+    key            = "lab2/terraform.tfstate"
+    region         = "ap-southeast-1"
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "6.39.0"
     }
   }
-}
-
-provider "aws" {
-  region = var.region
 }
